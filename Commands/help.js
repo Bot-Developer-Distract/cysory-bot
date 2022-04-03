feb5ae4431c5e7a57c735e5e998f1d6f3a156fdd
JOOJ09 <87592914+JOOJ09@users.noreply.github.com>
2022-04-03 14:29:29 +0700

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 exports.run = (client, message, args) => {
     const helpEmbed = new Discord.MessageEmbed()
     .setTitle("HELP BOARD :recycle: CYSORY COM")
-    .setDescription("Danh sách lệnh của bot Cysory \n Prefix của bot là: \`c/\` \n Cần thêm sự trợ giúp? hãy tham gia server bên dưới! \n Nhập \`c/help {tên lệnh}\` để xem kĩ thông tin của lệnh\nVì code có giới hạn nên thay vì bấm cachs 1 lần thì hãy bấm space 2 lần rồi mới mention nhé!")
+    .setDescription("Danh sách lệnh của bot Cysory \n Prefix của bot là: \`c/\` \n Cần thêm sự trợ giúp? hãy tham gia server bên dưới! \n Nhập \`c/help {tên lệnh}\` để xem kĩ thông tin của lệnh \n Vì code có giới hạn nên thay vì bấm cách 1 lần thì hãy bấm space 2 lần rồi mới mention nhé!")
     .addField("❯ Fun [3 lệnh]", "\`avatar\`  \`BKAV\` \`whois\`")
     .addField("❯ Info [3 lệnh]", "\`botinfo\` \`invite\` \`ping\`")
     .addField("❯ Code [0 lệnh]", "")
