e18aa56b659b95f3607ec946ef28ec18a5c5d226
JOOJ09 <87592914+JOOJ09@users.noreply.github.com>
2022-04-04 22:02:44 +0700

    Update whois

--- Commands/Fun/whois.js
+++ Commands/Fun/whois.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args) => {
     .setAuthor(`${Target.username}`, Target.displayAvatarURL({dynamic: true}))
     .setThumbnail(Target.displayAvatarURL({dynamic: true}))
     .addField("Thông tin người dùng", `Tên: **${Target.username}**\nID: **${Target.id}**\nTạo vào lúc: **${moment(Target.createdAt).format('Do MMMM YYYY, h:mm:ss a')}**\n**-** **${moment(Target.createdAt).startOf('day').fromNow()}**`, true)
-    .addField("Thông tin Server", `Tham gia server lúc: **${moment(Member.joinedAt).format('Do MMMM YYYY, h:mm:ss a')}**\n**-** **${moment(Member.joinedAt).startOf('day').fromNow()}**\nRoles: ${Member.roles.cache.map(r => r).join(' ').replace("@everyone", " ")}`, true)
+    .addField("Thông tin Server", `Tạo vào lúc: **${moment(Member.joinedAt).format('Do MMMM YYYY, h:mm:ss a')}**\n**-** **${moment(Member.joinedAt).startOf('day').fromNow()}**\nRoles: ${Member.roles.cache.map(r => r).join(' ').replace("@everyone", " ")}`, true)
     .setColor("RANDOM")
     message.channel.send({embeds:[Embed]})
 }
