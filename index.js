7de08f84ef24cf8edebf3edcaf9d88aae32e75c2
JOOJ09 <87592914+KravonCodetech@users.noreply.github.com>
2022-06-17 12:43:58 +0700

    Inital Commit

--- index.js
+++ index.js
@@ -53,52 +53,9 @@ client.on('message', message => {
 client.on('guildMemberAdd', async member => {
   const welcome = '986819503969165342'
   if(!member.guild) return
-
-  const canvas = Canvas.createCanvas(1770, 635)
-  const ctx = canvas.getContext('2d')
-  const background = await Canvas.loadImage(join(__dirname, './Image', 'parallax.jpg'))
-  ctx.drawImage(background, 0 ,0, canvas.width, canvas.height)
-  ctx.strokeStyle = '#FFFFFF'
-
-  ctx.strokeRect(0, 0, canvas.width, canvas.height)
-  var name = `${member.user.username}`
-  if(name.length >= 16) {
-      ctx.font = '100px Century Gothic'
-      ctx.fillStyle = '#ffffff'
-      ctx.fillText(name, 680, canvas.height / 2 - 1) 
-  } else {
-      ctx.font = '130px Century Gothic'
-      ctx.fillStyle = '#ffffff'
-      ctx.fillText(name, 680, canvas.height / 2 - 1)
-  }
-
-  var discrim = `#${member.user.discriminator}`
-  ctx.font = '130px Century Gothic'
-  ctx.fillStyle = '#75726f'
-  ctx.fillText(discrim, 1360, canvas.height / 2 + 13) 
-
-  var server = `${member.guild.name}` 
-  ctx.font = '80px Bauhaus 93'
-  ctx.fillStyle = '#ffffff'
-  ctx.fillText(server, 680, canvas.height / 2 - 100)
-
-  var count = `Bạn là thành viên thứ #${member.guild.memberCount}`
-  ctx.font = '80px Source Sans Pro SemiBold' 
-  ctx.fillStyle = '#82bd9e'
-  ctx.fillText(count, 690, canvas.height / 2 + 80)
-
-  ctx.beginPath()
-  ctx.arc(315, canvas.height / 2, 250, 0, Math.PI * 2, true)
-  ctx.closePath()
-  ctx.clip()
-
-  const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'png' }))
-  ctx.drawImage(avatar, 65, canvas.height / 2- 250, 500, 500)
-
-  const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'parallax.jpg')
-
   const channel = member.guild.channels.cache.get(welcome)
-  channel.send({content: `> Chào mừng ${member} đã đến với cộng đồng Artist của bọn mình\n> Hãy đọc kĩ luật lệ và bắt đầu khám phá nhé!`, files: [attachment]}) // Send Embed
+  const Embed4 = new Discord
+  channel.send({content: `> Chào mừng ${member} đã đến với cộng đồng Artist của bọn mình\n> Hãy đọc kĩ luật lệ và bắt đầu khám phá nhé!`, embeds: [Embed4]})
 })
 
 // tictactoe game
