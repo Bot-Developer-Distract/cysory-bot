03d4d5640c5d0b292a8d5d3b8bbbb07a5fe1cb9e
JOOJ09 <87592914+KravonCodetech@users.noreply.github.com>
2022-07-02 09:59:54 +0700

    Inital Commit

--- Commands/OwnerBot-only/task.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-   commands: 'task',
-   callback: (message) => {
-      const User = message.member
-      const db = require('quick.db')
-      const note = db.fetch(`note_${User.id}`)
-      message.channel.send(`${note}`)
-   }
-}
